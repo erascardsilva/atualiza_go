b30b98c68d7a56ed7d9af680f6d730b67a311af6
erascardsilva <erasmo@vmi-informatica.com.br>
2026-03-31 19:19:22 -0300

    refactor: reversao vital para arquitetura original Classic abolindo modificacoes de gpu e dll em decorrencia do colapso no Wayland

--- main.go
+++ main.go
@@ -5,26 +5,16 @@ package main
 
 import (
 	"embed"
-	"os"
 
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
 	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
-	"github.com/wailsapp/wails/v2/pkg/options/linux"
 )
 
 //go:embed all:frontend/dist
 var assets embed.FS
 
 func main() {
-	// OBRIGA O STARTUP GTK A CORTAR TODA RENDERIZAÇÃO WAYLAND/HARDWARE EGL
-	// Essencial para sobreviver ao bloqueio do Webkit/Core22 ACPI e EGL.
-	os.Setenv("WEBKIT_DISABLE_SANDBOX", "1")
-	os.Setenv("WEBKIT_DISABLE_COMPOSITING_MODE", "1")
-	os.Setenv("LIBGL_ALWAYS_SOFTWARE", "1")
-	// EXTERMINA MÓDULOS DE BIP (canberra-gtk) DO GTK2 injetados pela loja para não matarem a GLib GTK3 do Wails.
-	os.Setenv("GTK_MODULES", "")
-	
 	app := NewApp()
 
 	err := wails.Run(&options.App{
@@ -40,10 +30,6 @@ func main() {
 		OnStartup:        app.startup,
 		Bind: []interface{}{
 			app,
-		},
-		Linux: &linux.Options{
-			WindowIsTranslucent: false,
-			WebviewGpuPolicy:    linux.WebviewGpuPolicyOnDemand,
 		},
 	})
 
